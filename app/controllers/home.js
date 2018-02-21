0dcb40abaab44060a43a743366915d9833c70fef
Dan Goodson <dan.goodson@saywell.co.uk>
2018-02-21 13:20:33 +0000

    first commit

---
+++ app/controllers/home.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+  router = express.Router(),
+  Article = require('../models/article'),
+  config = require('../../config/config');
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.get('/', function (req, res, next) {
+  var articles = [new Article(), new Article()];
+    res.render('index', {
+      title: config.app.name,
+      articles: articles
+    });
+});
